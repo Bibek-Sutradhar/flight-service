c8355b6e916fdc7def943e420831d20dd2e4e626
BibekSutradhar <BibekSutradhar@host.docker.internal>
2020-05-07 23:58:54 +0530

    initial commit

---
+++ flightservice/src/main/java/com/ibm/flightservice/controller/FlightSearchController.java
@@ -0,0 +1,32 @@
+package com.ibm.flightservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.flightservice.dto.FlightRequestDTO;
+import com.ibm.flightservice.dto.FlightSearchResponseDTO;
+import com.ibm.flightservice.service.FlightSearchService;
+
+@CrossOrigin(origins="*")
+@RestController
+public class FlightSearchController {
+	
+	@Autowired
+	FlightSearchService flightService;
+	 
+	@PostMapping(value="/flight")
+	public ResponseEntity<FlightSearchResponseDTO> getFlightDetails( @RequestBody FlightRequestDTO flightRequestDTO) {			
+		
+		 
+        return ResponseEntity.ok().body(flightService.flightSearch(flightRequestDTO)); 
+    }
+}
+	
+
+
